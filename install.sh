44029afa00b03c3866dddb01d8cc7a8f9c60f264
maritims <maritim@gmail.com>
2026-01-14 21:38:33 +0100

    Reorganized nginx configuration.

--- install.sh
+++ install.sh
@@ -4,23 +4,28 @@
 
 shopt -s nullglob
 
-# Local scrcipts
+# Local scripts
 mkdir -p ~/.local/bin
 for f in .local/bin/*; do
 	echo "Creating symlink: $f"
-	ln -sf "$f" ~/.local/bin/
+	ln -sf "$f" "$PWD/.local/bin/"
 done
 
 # Podman container files
 mkdir -p ~/.config/containers/systemd
 for f in .config/containers/systemd/*; do
 	echo "Creating symlink: $f"
-	ln -sf "$f" ~/.config/containers/systemd/
+	ln -sf "$f" "$PWD/.config/containers/systemd/"
 done
 
 # systemd user timers
 mkdir -p ~/.config/systemd/user
 for f in .config/systemd/user/*; do
 	echo "Creating symlink: $f"
-	ln -sf "$f" ~/.config/systemd/user/
+	ln -sf "$f" "$PWD/.config/systemd/user/"
 done
+
+# Nginx configuration
+mkdir -p ~/.config/nginx
+echo "Creating symlink: .config/nginx/nginx.conf"
+ln -sf "$PWD/.config/nginx/nginx.conf" ~/.config/nginx/nginx.conf
